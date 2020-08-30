ea6367815a32e520807f988adca83d5d59f33c8b
Patel Dhruvin Pravinbhai <dhruvinpatel1831@gmail.com>
2020-08-30 22:31:00 +0530

    React app created, scripts added

--- routes/api/items.js
+++ routes/api/items.js
@@ -28,7 +28,7 @@ router.post('/', (req, res) =>{
 // @access Public
 router.delete('/:id', (req, res) =>{
    Item.findById(req.params.id)
-    .then(item => Item.remove().then(res.json({ success: true})))
+    .then(item => Item.deleteOne().then(res.json({ success: true})))
     .catch(err => res.status(404).json({ success: false}))
 })
 
