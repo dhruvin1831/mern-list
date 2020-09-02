cb642fef04bd375441d0d19df33166d6afe5c237
Patel Dhruvin Pravinbhai <dhruvinpatel1831@gmail.com>
2020-09-02 20:21:02 +0530

    React frontend added - navbar added

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from "reactstrap";
+
+class AppNavbar extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="primary" dark expand="md" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">List</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/dhruvin1831">
+                    Components
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
